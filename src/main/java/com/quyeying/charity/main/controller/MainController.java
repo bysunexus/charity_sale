09fba76fdddc06b1eb91e55bb9569421f4b0644c
kakotor@gmail.com <kakotor@gmail.com>
2014-06-05 22:16:56 +0800

    主页控制器

---
+++ src/main/java/com/quyeying/charity/main/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.quyeying.charity.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * User: bysun
+ * Date: 2014/6/3
+ * Time: 10:27
+ */
+@RequestMapping("/")
+@Controller
+public class MainController {
+
+}
