e00913bb91312179d17af1eb46e93ab262ebbeb2
Watson W <waston.wang.email@gmail.com>
2014-06-12 12:23:50 +0800

    [Watson W] 修改登录页

--- src/main/java/com/quyeying/charity/login/controller/LoginController.java
+++ src/main/java/com/quyeying/charity/login/controller/LoginController.java
@@ -1,5 +1,10 @@
 package com.quyeying.charity.login.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
 /**
  * charity_sale
  * com.quyeying.charity.login.controller
@@ -8,8 +13,47 @@ package com.quyeying.charity.login.controller;
  * Date: 2014/6/12
  * Time: 10:56
  */
+@Controller
 public class LoginController {
 
+    /**
+     * 登录方法
+     *
+     * @param userName 用户名
+     * @param passWord 密码
+     *
+     * @return 返回首页
+     */
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public ModelAndView login(String userName, String passWord) {
+        if (this.checkParams(new String[]{userName, passWord})) {
+            ModelAndView mav = new ModelAndView("index");
+            mav.addObject("username",userName);
+            mav.addObject("password", passWord);
+            return mav;
+        }
+        return new ModelAndView("login");
+    }
 
+    @RequestMapping(value = "toLogin")
+    public ModelAndView toLogin() {
+        return new ModelAndView("login");
+    }
+
+    /**
+     * 验证参数是否为空
+     *
+     * @param params 校验参数
+     *
+     * @return 失败返回false 成功返回true
+     */
+    private boolean checkParams(String[] params) {
+        for (String param : params) {
+            if (param.equals("") || param.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
