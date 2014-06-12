9de018ddbe931e90822b5cfb9f013c9dd946b9f9
Watson W <waston.wang.email@gmail.com>
2014-06-12 11:08:36 +0800

    [Watson W] 添加login controller

---
+++ src/main/java/com/quyeying/charity/login/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.quyeying.charity.login.controller;
+
+/**
+ * charity_sale
+ * com.quyeying.charity.login.controller
+ * Created with IntelliJ IDEA.
+ * User: Watson
+ * Date: 2014/6/12
+ * Time: 10:56
+ */
+public class LoginController {
+
+
+
+}
