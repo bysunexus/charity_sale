755008ba8917a4f93550777d313012d340b1b054
Watson W <waston.wang.email@gmail.com>
2014-08-06 11:04:22 +0800

    Merge branch 'dev' of http://git.oschina.net/kakotor/charity_sale into dev
    Conflicts:
    	src/main/java/com/quyeying/charity/goods/service/GoodsRepositoryCustom.java
    	src/main/java/com/quyeying/charity/goods/service/GoodsRepositoryImpl.java

--- src/main/java/com/quyeying/charity/report/controller/TotalReportController.java
+++ src/main/java/com/quyeying/charity/report/controller/TotalReportController.java
@@ -85,6 +85,31 @@ public class TotalReportController {
         List<Goods> list = repo.findByGoodsNum((null == goodsNum || "null".equals(goodsNum)) ? "" : goodsNum);
         InputStream inp = this.getClass().getClassLoader().getResourceAsStream("template/totalTemplate.xlsx");
 
+        XSSFWorkbook wb = getXssfSheets(list, inp);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        wb.write(stream);
+        stream.flush();
+
+        HttpHeaders headers = new HttpHeaders();
+        String fileName = new String("爱心义卖销售报表.xlsx".getBytes("UTF-8"), "iso-8859-1");
+        headers.setContentDispositionFormData("attachment", fileName);
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        byte[] data = stream.toByteArray();
+        IOUtils.closeQuietly(stream);
+        return new ResponseEntity<byte[]>(data, headers, HttpStatus.CREATED);
+
+    }
+
+    /**
+     * 获取XSSFWorkbook
+     * @param list 数据源
+     * @param inp InputStream
+     * @return XSSFWorkbook
+     * @throws IOException
+     */
+    @SuppressWarnings("ConstantConditions")
+    private XSSFWorkbook getXssfSheets(List<Goods> list, InputStream inp) throws IOException {
         XSSFWorkbook wb = new XSSFWorkbook(inp);
         int a = 2, b = 2, c = 2, d = 2, e = 2;
 
@@ -118,113 +143,117 @@ public class TotalReportController {
                 if (null == row)
                     continue;
 
-                int idx = 0;
-
-                Cell cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getGoodsNum());
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getPersonName());
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getPersonPhone());
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getGoodsName());
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getGoodsCount());
-                idx++;
-
-                if ("E".equals(item.getGoodsType())) {
-                    cell = row.getCell(idx);
-                    if (null == cell)
-                        cell = row.createCell(idx);
-                    cell.setCellType(Cell.CELL_TYPE_STRING);
-                    cell.setCellValue(item.getGoodsPrice());
-                    idx++;
-                }
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                int saleCount = 0;
-                if (null != item.getSaleInfos()) {
-                    for (Goods.SaleInfo saleInfo : item.getSaleInfos()) {
-                        saleCount += saleInfo.getSaleCount();
-                    }
-                }
-                cell.setCellValue(saleCount);
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                int saleMoney = 0;
-                if (null != item.getSaleInfos()) {
-                    for (Goods.SaleInfo saleInfo : item.getSaleInfos()) {
-                        saleMoney += saleInfo.getSaleMoney();
-                    }
-                }
-                cell.setCellValue(saleMoney);
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getRemark());
-                idx++;
-
-                cell = row.getCell(idx);
-                if (null == cell)
-                    cell = row.createCell(idx);
-                cell.setCellType(Cell.CELL_TYPE_STRING);
-                cell.setCellValue(item.getGoodsCount() - saleCount);
-                idx++;
+                buildCell(item, row);
 
             }
         }
-
-        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-        wb.write(stream);
-        stream.flush();
-
-        HttpHeaders headers = new HttpHeaders();
-        String fileName = new String("爱心义卖销售报表.xlsx".getBytes("UTF-8"), "iso-8859-1");
-        headers.setContentDispositionFormData("attachment", fileName);
-        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-        byte[] data = stream.toByteArray();
-        IOUtils.closeQuietly(stream);
-        return new ResponseEntity<byte[]>(data, headers, HttpStatus.CREATED);
-
+        return wb;
     }
 
-    private Row getRow(Workbook wb, int idx, int a) {
-        Row row = wb.getSheetAt(idx).getRow(a);
+    /**
+     * 构建单元格数据
+     * @param item goods
+     * @param row row
+     */
+    private void buildCell(Goods item, Row row) {
+        int idx = 0;
+
+        Cell cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getGoodsNum());
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getPersonName());
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getPersonPhone());
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getGoodsName());
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getGoodsCount());
+        idx++;
+
+        if ("E".equals(item.getGoodsType())) {
+            cell = row.getCell(idx);
+            if (null == cell)
+                cell = row.createCell(idx);
+            cell.setCellType(Cell.CELL_TYPE_STRING);
+            cell.setCellValue(item.getGoodsPrice());
+            idx++;
+        }
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        int saleCount = 0;
+        if (null != item.getSaleInfos()) {
+            for (Goods.SaleInfo saleInfo : item.getSaleInfos()) {
+                saleCount += saleInfo.getSaleCount();
+            }
+        }
+        cell.setCellValue(saleCount);
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        int saleMoney = 0;
+        if (null != item.getSaleInfos()) {
+            for (Goods.SaleInfo saleInfo : item.getSaleInfos()) {
+                saleMoney += saleInfo.getSaleMoney();
+            }
+        }
+        cell.setCellValue(saleMoney);
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getRemark());
+        idx++;
+
+        cell = row.getCell(idx);
+        if (null == cell)
+            cell = row.createCell(idx);
+        cell.setCellType(Cell.CELL_TYPE_STRING);
+        cell.setCellValue(item.getGoodsCount() - saleCount);
+        idx++;
+    }
+
+    /**
+     * 获取row
+     * @param wb Workbook
+     * @param idx idx
+     * @param rowNum rowNum
+     * @return
+     */
+    private Row getRow(Workbook wb, int idx, int rowNum) {
+        Row row = wb.getSheetAt(idx).getRow(rowNum);
         if (null == row) {
-            row = wb.getSheetAt(idx).createRow(a);
+            row = wb.getSheetAt(idx).createRow(rowNum);
         }
         return row;
     }
