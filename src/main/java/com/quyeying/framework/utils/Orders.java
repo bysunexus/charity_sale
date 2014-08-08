01c5d6ed20f6a6bacd05ce3ef4d2cc85a21f9d24
bysun <kakotor@gmail.com>
2014-08-08 23:31:21 +0800

    排序字段生成修改 pom更新

--- src/main/java/com/quyeying/framework/utils/Orders.java
+++ src/main/java/com/quyeying/framework/utils/Orders.java
@@ -1,5 +1,8 @@
 package com.quyeying.framework.utils;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User: bysun
  * Date: 2014/8/7
@@ -7,12 +10,27 @@ package com.quyeying.framework.utils;
  */
 public class Orders {
 
+    private final static Map<Character, Character> CHARS = new HashMap<Character, Character>() {{
+        put('A', '1');
+        put('B', '2');
+        put('C', '3');
+        put('D', '4');
+        put('E', '5');
+    }};
+
     public static long longOrder(String key) {
+
         long order = 0;
-        if (null != key)
-            for (char c : key.toCharArray()) {
-                order += c;
+        if (null != key && key.length() < 4) {
+            char[] chars = key.toCharArray();
+            char[] tmp = new char[]{'0', '0', '0', '0'};
+            tmp[0] = null == CHARS.get(chars[0]) ? '0' : CHARS.get(chars[0]);
+            int idx = 3;
+            for (int i = chars.length - 1; i >= 1; i--) {
+                tmp[idx--] = chars[i];
             }
+            order = Long.parseLong(new String(tmp));
+        }
         return order;
     }
 }
